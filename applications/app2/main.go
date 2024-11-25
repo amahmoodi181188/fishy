288c14d3585ad2230ca3f01ec7371406a9c5593f
Ali Mahmoodi <alimahmoodi@Alis-Air.fritz.box>
2024-11-25 15:09:37 +0100

    add required files and sync with Github repo

---
+++ applications/app2/main.go
@@ -0,0 +1,17 @@
+// main.go for App 2
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "When in doubt, fish!")
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
+}
+
