f875fcc79371a8383f80b892848b96f8ae75a41b
Ali Mahmoodi <alimahmoodi@Alis-Air.fritz.box>
2024-11-26 16:44:21 +0100

    update Dockerfiles

--- app1/main.go
+++ app1/main.go
@@ -1,17 +1,7 @@
-// main.go for App 1
 package main
 
-import (
-    "fmt"
-    "net/http"
-)
-
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "There's no such thing as a bad day when you're fishing.")
-}
+import "fmt"
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+    fmt.Println("There's no such thing as a bad day when you're fishing.")
 }
-
